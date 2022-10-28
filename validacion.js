d5bf064aad6c61ae9659a433c3fb108dba5c2833
Amin Arce <aminf.arce@gmail.com>
2022-10-27 23:58:15 -0300

    Cambio de seccion contacto

--- validacion.js
+++ validacion.js
@@ -2,11 +2,10 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
+
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+
 }
 
 const check = {
@@ -50,10 +49,7 @@ const validar = (e) => {
                 check['Email']=false;
             }
         break;
-        //case "mensaje":
-        //    if (formulario.mensaje.value == 0) {
-        //        document.getElementById('group_mensaje').classList.add('form_group-incorrecto');
-        //break;
+
     }
 }
 
